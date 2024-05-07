10d686542b18ab42c8027a0ff65e850a477f9df0
Beknar Bakytbekuly <bbeknar@gmail.com>
2024-05-06 19:31:00 -0500

    submitted longest substring

---
+++ longest-substring-without-repeating-character.js
@@ -0,0 +1,15 @@
+var lengthOfLongestSubstring = function (s) {
+  if (!s) return 0;
+  let max = 1;
+  for (let i = 0; i < s.length; i++) {
+    //optimized a bit, we don't need to go further if our maximum is already more than number of characters left
+    if (s.length - i < max) break;
+    for (let j = i + 1; j <= s.length; j++) {
+      const curMax = new Set(s.slice(i, j).split(""));
+      //if there is any repeating character, we don't need to check other part of the string
+      if (curMax.size < j - i) break;
+      else if (curMax.size > max) max = curMax.size;
+    }
+  }
+  return max;
+};
